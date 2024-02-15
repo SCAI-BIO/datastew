ef45f3bd858da37fa00605d5ac1a6d0b089908e5
TimAdams84 <tim-adams@gmx.net>
2024-02-15 16:17:31 +0100

    Add fastapi, dockerfile & integrate into ci

---
+++ index/api/routes.py
@@ -0,0 +1,43 @@
+import logging
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+
+app = FastAPI(
+    title="INDEX",
+    description="Intelligent data steward toolbox using Large Language Model embeddings "
+                "for automated Data-Harmonization .",
+    version="0.0.1",
+    terms_of_service="https://www.scai.fraunhofer.de/",
+    contact={
+        "name": "Dr. Marc Jacobs",
+        "email": "marc.jacobs@scai.fraunhofer.de",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger = logging.getLogger("uvicorn.info")
+
+
+@app.get("/", include_in_schema=False)
+def swagger_redirect():
+    return RedirectResponse(url='/docs')
+
+
+@app.get("/version", tags=["info"])
+def get_current_version():
+    return app.version
