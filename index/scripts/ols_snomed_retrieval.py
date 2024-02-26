d3fac5ab9e865e91a26a0fdf9c26b8f6b0d03c99
TimAdams84 <tim-adams@gmx.net>
2024-02-26 10:17:36 +0100

    Add OLS terminology import

--- index/scripts/ols_snomed_retrieval.py
+++ index/scripts/ols_snomed_retrieval.py
@@ -2,7 +2,7 @@ from index.embedding import MPNetAdapter
 from index.process.ols import OLSTerminologyImportTask
 from index.repository.sqllite import SQLLiteRepository
 
-repository = SQLLiteRepository()
+repository = SQLLiteRepository(name="snomed")
 embedding_model = MPNetAdapter()
 
 task = OLSTerminologyImportTask(repository, embedding_model, "SNONMED CT", "snomed")
