acc3ebf58b925b309572e20916a8c65e7592634e
Tim Adams <tim-adams@gmx.net>
2024-03-01 10:46:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import io
+import os
+
+from setuptools import setup
+
+DESCRIPTION = 'Intelligent data steward toolbox using Large Language Model embeddings for automated Data-Harmonization.'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name='index',
+    version='0.0.2',
+    packages=['index'],
+    url='https://github.com/SCAI-BIO/index',
+    license='Apache-2.0 license"',
+    author='Tim Adams',
+    author_email='tim.adams@scai.fraunhofer.de',
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+)
