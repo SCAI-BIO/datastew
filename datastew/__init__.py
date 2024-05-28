af3938492a9ea93e23a7ef764755d9951315a3cc
Tim Adams <tim-adams@gmx.net>
2024-05-28 14:45:44 +0200

    Update __init__.py

---
+++ datastew/__init__.py
@@ -0,0 +1,11 @@
+from datastew import visualization
+from datastew import mapping
+from datastew import embedding
+
+from datastew.process import mapping
+from datastew.process import ols
+from datastew.process import parsing
+
+from datastew.repository import base
+from datastew.repository import model
+from from datastew.repository import sqllite
