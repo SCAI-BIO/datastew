df67ef7bf9ffc60b8c8949a52beb0c4bb704dba1
Tim Adams <tim-adams@gmx.net>
2024-05-29 10:56:31 +0200

    Update __init__.py

--- datastew/__init__.py
+++ datastew/__init__.py
@@ -1,11 +1,8 @@
+# root dir files 
 from datastew import visualisation
 from datastew import mapping
 from datastew import embedding
 
-from datastew.process import mapping
-from datastew.process import ols
-from datastew.process import parsing
-
-from datastew.repository import base
-from datastew.repository import model
-from datastew.repository import sqllite
+# packages
+from datastew import process
+from datastew import repository
