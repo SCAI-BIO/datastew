f653a4a53a5f1873345daa16d78d5c1e1abe10d9
TimAdams84 <tim-adams@gmx.net>
2024-07-15 13:59:45 +0200

    Add WeaviateRepository to package init

--- datastew/repository/__init__.py
+++ datastew/repository/__init__.py
@@ -1,9 +1,11 @@
 from .model import Terminology, Concept, Mapping
 from .sqllite import SQLLiteRepository
+from .weaviate import WeaviateRepository
 
 __all__ = [
     "Terminology",
     "Concept",
     "Mapping",
-    "SQLLiteRepository"
+    "SQLLiteRepository",
+    "WeaviateRepository"
 ]
