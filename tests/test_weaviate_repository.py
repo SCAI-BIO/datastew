3f453a158679dac00fc22005b28174542e87658d
TimAdams84 <tim-adams@gmx.net>
2024-07-29 16:03:58 +0200

    Re-activate weaviate test

--- tests/test_weaviate_repository.py
+++ tests/test_weaviate_repository.py
@@ -7,7 +7,7 @@ from datastew.repository.weaviate import WeaviateRepository
 
 
 class Test(TestCase):
-    @unittest.skip("currently broken on github workflows")
+
     def test_repository(self):
 
         repository = WeaviateRepository(mode="disk", path="db")
