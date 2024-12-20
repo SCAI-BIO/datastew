eefefdefebfb0306f4e763b0abd903d47ccba9ee
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-12-20 11:07:04 +0100

    chores: typo

--- datastew/scripts/ols_snomed_retrieval.py
+++ datastew/scripts/ols_snomed_retrieval.py
@@ -2,9 +2,9 @@ from datastew.embedding import MPNetAdapter
 from datastew.process.ols import OLSTerminologyImportTask
 from datastew.repository.sqllite import SQLLiteRepository
 
-repository = SQLLiteRepository(name="snomed")
+repository = SQLLiteRepository()
 embedding_model = MPNetAdapter()
 
-task = OLSTerminologyImportTask(repository, embedding_model, "SNONMED CT", "snomed")
+task = OLSTerminologyImportTask(repository, embedding_model, "SNOMED CT", "snomed")
 task.process()
 print("done")
