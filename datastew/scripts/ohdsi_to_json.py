375b53011c8c87636be93adca52bf0ebd0e9e600
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-03-27 18:53:34 +0100

    refactor: variable naming for consistency

--- datastew/scripts/ohdsi_to_json.py
+++ datastew/scripts/ohdsi_to_json.py
@@ -2,6 +2,6 @@ from datastew.embedding import Vectorizer
 from datastew.process.jsonl_adapter import WeaviateJsonlConverter
 
 json_converter = WeaviateJsonlConverter("resources/results")
-embedding_model = Vectorizer()
+vectorizer = Vectorizer()
 
-json_converter.from_ohdsi("resources/CONCEPT.csv", embedding_model)
+json_converter.from_ohdsi("resources/CONCEPT.csv", vectorizer)
