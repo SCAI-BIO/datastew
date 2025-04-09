4f72cbaf096d6695274f0922f80357f14ac18a4a
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-04-09 14:55:52 +0200

    docs: simplify code snippet

--- datastew/scripts/ohdsi_to_json.py
+++
@@ -1,7 +0,0 @@
-from datastew.embedding import Vectorizer
-from datastew.process.jsonl_adapter import WeaviateJsonlConverter
-
-json_converter = WeaviateJsonlConverter("resources/results")
-vectorizer = Vectorizer()
-
-json_converter.from_ohdsi("resources/CONCEPT.csv", vectorizer)
