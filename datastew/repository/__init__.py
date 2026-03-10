116fa0c1c3e203c03777056a68956bcc07e2f335
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-10 12:10:09 +0100

    refactor: remove weaviate

--- datastew/repository/__init__.py
+++ datastew/repository/__init__.py
@@ -1,6 +1,5 @@
 from .model import Concept, Mapping, Terminology
 from .postgresql import PostgreSQLRepository
 from .sqllite import SQLLiteRepository
-from .weaviate import WeaviateRepository
 
-__all__ = ["Terminology", "Concept", "Mapping", "SQLLiteRepository", "WeaviateRepository", "PostgreSQLRepository"]
+__all__ = ["Terminology", "Concept", "Mapping", "SQLLiteRepository", "PostgreSQLRepository"]
